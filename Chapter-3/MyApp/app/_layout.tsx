558efb7b9798be44ebc90f39555650833460387a
Susa <susa.d.dilmin@gmail.com>
2025-01-08 16:21:36 +0530

    Move MyApp to Chapter-2 & Create Chapter-3

---
+++ Chapter-3/MyApp/app/_layout.tsx
@@ -0,0 +1,44 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+// Stack - is a component that allows you to define a stack of screens in your app.
+//Like when you create a page it's name whill shown above ...
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="(coffee)" options={{ headerShown: false }} /> */}
+        <Stack.Screen name='index' options={{ title : "Home" , headerShown: false }} /> 
+        <Stack.Screen name='explore' options={{ title : "Explore more" }} />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
+  );
+}
